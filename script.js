a0c76453510b628669c888319d727d1af902277d
DagiH22 <dagih22@gmail.com>
2024-03-26 18:40:37 +0300

    "fixed the position of container" "fixed the position of the buttons" "added backround image" "modified script.js to change the image when shrinked"

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let img1 =document.querySelector(".img1")
 let img2 =document.querySelector(".img2")
 let img3 =document.querySelector(".img3")
 let img4 =document.querySelector(".img4")
+let plus = document.querySelectorAll(".class")
 let oneExpand = false
 let twoExpand = false
 let threeExpand = false
@@ -15,40 +16,49 @@ function expand (num){
         if (oneExpand){
             one.style.display = "none";
             oneExpand= false
+            img1.setAttribute('src', "assets/images/icon-plus.svg")
         }
         else{
             one.style.display = "block";
             oneExpand= true
+            img1.setAttribute('src', "assets/images/icon-minus.svg")
+            
         }
      }
      else if (num == 2){
         if (twoExpand){
             two.style.display = "none";
             twoExpand =false
+            img2.setAttribute('src', "assets/images/icon-plus.svg")
         }
         else{
             two.style.display = "block";
             twoExpand =true
+            img2.setAttribute('src', "assets/images/icon-minus.svg")
         }
      }
      else if (num == 3){
         if (threeExpand){
             three.style.display = "none";
             threeExpand =false
+            img3.setAttribute('src', "assets/images/icon-plus.svg")
         }
         else{
             three.style.display = "block";
             threeExpand =true
+            img3.setAttribute('src', "assets/images/icon-minus.svg")
         }
      }
      else if (num == 4){
         if (fourExpand){
             four.style.display = "none";
             fourExpand =false
+            img4.setAttribute('src', "assets/images/icon-plus.svg")
         }
         else{
             four.style.display = "block";
             fourExpand =true
+            img4.setAttribute('src', "assets/images/icon-minus.svg")
         }
      }
 }
